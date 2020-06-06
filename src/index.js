4344df214047565ed1fe0130671f2d9eb81cc96c
deividdotum <deivid@dotum.com.br>
2020-06-05 20:43:52 -0400

    iniciando sem sucesso mas temos ideia de usar o google documents api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+const docx = require("./readDocx");
+
+const directoryPath = path.join(__dirname, "docks-directory");
 function main() {
   console.log("DOCX-19");
+
+  console.log("Read directory: ", directoryPath);
+  fs.readdir(directoryPath, function (err, files) {
+    if (err) return console.log(err);
+    files.forEach((el) => {
+      console.log("FILE: ", el);
+      docx.extract(path.join(directoryPath, el)).then((data) => {
+        console.log("CONTENT: ", data);
+      });
+    });
+  });
 }
+
+main();
